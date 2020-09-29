591bfc6625e3fbfdd4b874e2ba46eaeb9db8a45d
Attila Gabor <gabor.attila87@gmail.com>
2020-09-29 13:41:46 +0200

    main API updates

--- R/preprocess_COSMOS.R
+++ R/preprocess_COSMOS.R
@@ -15,10 +15,25 @@
 #' in the PKN and values are from \{1, 0, -1\}. Continuous data will be 
 #' discretized using the \link\code{sign} function.  
 #' @param metabolic_data numerical vector, where names are metabolic nodes 
-#' in the PKN and values are continuous values. These values are compared to 
-#' with the the simulation [? range of values]
-#' @param expression_data (optional) numerical vector, where names are gene
-#' names and values are from \{1,-1\}
+#' in the PKN and values are continuous values that represents log2 fold change 
+#' or t values from a differential analysis. These values are compared to 
+#' the simulation results (simulated nodes can take value -1, 0 or 1)
+#' @param diff_expression_data (optional) numerical vector that represents the 
+#' results of a differential gene expression analysis. Names are gene
+#' names using EntrezID starting with an X and values are log fold change or
+#'  t-values. We use the `diff_exp_threshold` parameter to decide which genes 
+#'  changed significantly. Genes with NA values are considered none expressed
+#'  and they will be removed from the TF-gene expression interactions. 
+#' @param diff_exp_threshold threshold parameter (default 1) used to binarize
+#'  the values of `diff_expression_data`. 
+#' @param maximum_network_depth integer > 0 (default: 8). Nodes that are further 
+#' than `maximum_network_depth` steps from the signaling nodes on the directed
+#' graph of the PKN are considered non-reachable and are removed. 
+#' @param remove_unexpressed_nodes if TRUE (default) removes nodes from the PKN 
+#' that are not expressed, see input \code{expressed_genes}.
+#' @param expressed_genes character vector. Names of nodes that are expressed. By 
+#' default we consider all the nodes that appear in \code{diff_expression_data} with
+#' a numeric value (i.e. nodes with NA are removed) 
 #' @param filter_tf_gene_interaction_by_optimization (default:TRUE), if TRUE then runs 
 #' a network optimization that estimates TF activity not included in the inputs
 #' and checks the consistency between the estimated activity and change in gene 
@@ -30,20 +45,26 @@
 #' @param time_limit argument passed to \link\code{CARNIVAL::runCARNIVAL}
 #' used if filter_tf_gene_interaction_by_optimization is TRUE
 #' @export
-#' @import dorothea biomaRt igraph dplyr
+#' @import dplyr
 #' @return named list with the following members: 
 #'  - `meta_network`  filtered PKN
 #'  - `tf_regulon`  TF - target regulatory network
 #'  - `signaling_data_bin` binarised signaling data 
 #'  - `metabolic_data`  metabolomics data
-#'  - `expression_data_bin`  binarised gene expression data 
+#'  - `diff_expression_data_bin`  binarized gene expression data 
 #'  - `optimized_network` initial optimized network if filter_tf_gene_interaction_by_optimization is TRUE. 
-#'
+#' @seealso [load_meta_pkn()] for meta PKN, 
+#' [load_tf_regulon_dorothea()] for tf regulon,
+#' [genesymbols_to_entrezid()] for gene conversion. 
 preprocess_COSMOS <- function(meta_network = load_meta_pkn(),
                               tf_regulon = load_tf_regulon_dorothea(),
                               signaling_data,
                               metabolic_data,
-                              expression_data,
+                              diff_expression_data, 
+                              diff_exp_threshold  = 1,
+                              maximum_network_depth = 8,
+                              expressed_genes =  names(diff_expression_data)[!is.na(diff_expression_data)],
+                              remove_unexpressed_nodes = TRUE,
                               filter_tf_gene_interaction_by_optimization = TRUE,
                               solver_path = NULL, 
                               solver = "cplex",
@@ -54,32 +75,57 @@ preprocess_COSMOS <- function(meta_network = load_meta_pkn(),
                         tf_regulon,
                         signaling_data,
                         metabolic_data,
-                        expression_data)
+                        diff_expression_data)
+    
+    check_gene_names(signaling_data,diff_expression_data,expressed_genes)
     
     # Check overlap among node names in the inputs
     check_network_data_coverage(meta_network,
                                 tf_regulon,
                                 signaling_data,
                                 metabolic_data,
-                                expression_data)
+                                diff_expression_data)
     
-    # Preprocess PKN
+    # Preprocess PKN ----
+    
+    # filter nodes that do not appear in Gene-expression data. 
+    # if the gene has a t-value --> it is expressed. 
+    if(remove_unexpressed_nodes){
+        meta_network <- filter_pkn_expressed_genes(expressed_genes_entrez=expressed_genes,
+                                                   meta_pkn=meta_network)
+        # After modifying the PKN, inputs/measured nodes might get lost, we remove
+        signaling_data = filter_input_nodes_not_in_pkn(pkn = meta_network,
+                                                       data = signaling_data)
+        metabolic_data = filter_input_nodes_not_in_pkn(pkn = meta_network,
+                                                       data = metabolic_data)
+    }
+    
     # - cut unreachable nodes from inputs
     meta_network <- keep_downstream_neighbours(
         network = meta_network,
-        n_steps =  8,
+        n_steps =  maximum_network_depth, 
         starting_nodes = names(signaling_data))
+    # report the number of steps. Make it as a user input. 
+    
+    
+    # After modifying the PKN, inputs/measured nodes might get lost, we remove
+    signaling_data = filter_input_nodes_not_in_pkn(pkn = meta_network,
+                                                  data = signaling_data)
+    metabolic_data = filter_input_nodes_not_in_pkn(pkn = meta_network,
+                                                   data = metabolic_data)
+
     
     
     # Filter TF -> target interaction from PKN if target expression not changing
-    gene_expression_binarized <- binarize_with_sign(expression_data,
-                                                    threshold = 1)
+    diff_expression_data_bin <- binarize_with_sign(diff_expression_data,
+                                                    threshold = diff_exp_threshold)
+    # make this threshold accessible for the user. by default should be 1. 
     
     filtered_meta_network <- filter_transcriptional_regulations(
-        network = meta_network[,1:3], 
-        gene_expression_binarized = gene_expression_binarized,
+        network = meta_network, 
+        gene_expression_binarized = diff_expression_data_bin,
         signaling_data  = signaling_data,
-        tf_regulon=tf_regulon[,c("tf","sign","target")])
+        tf_regulon = tf_regulon)
     
     # run CARNIVAL from signaling to metabolism,
     # this may estimate the activity of other TF-s.
@@ -94,11 +140,12 @@ preprocess_COSMOS <- function(meta_network = load_meta_pkn(),
                                                mipGAP = 0.2)
         
         # get the estimated activity of TFs from CARNIVAL results
-        estimated_TF_activity <- get_TF_activity_from_CARNIVAL(CARNIVAL_results, tf_regulon$tf)
+        estimated_TF_activity <- get_TF_activity_from_CARNIVAL(CARNIVAL_results,
+                                                               tf_regulon$tf)
         
         filtered_meta_network <- filter_transcriptional_regulations(
             network = filtered_meta_network, 
-            gene_expression_binarized = binarize_with_sign(expression_data,
+            gene_expression_binarized = binarize_with_sign(diff_expression_data,
                                                            threshold = 1),
             signaling_data  = estimated_TF_activity,
             tf_regulon=tf_regulon[,c("tf","sign","target")])
@@ -112,6 +159,6 @@ preprocess_COSMOS <- function(meta_network = load_meta_pkn(),
                     tf_regulon = tf_regulon,
                     signaling_data_bin = sign(signaling_data),
                     metabolic_data = metabolic_data,
-                    expression_data = gene_expression_binarized,
+                    diff_expression_data_bin = diff_expression_data_bin,
                     optimized_network = CARNIVAL_results)
 }
---
+++ R/run_COSMOS_signaling_to_metabolism.R
@@ -0,0 +1,71 @@
+#' run_COSMOS_signaling2metabolism
+#' 
+#' runs COSMOS from signaling to metabolism. This function finds a subset of the
+#' prior knowledge network based on optimisation that (1) includes the most 
+#' measured and input nodes and (2) which is in agreement with the data. 
+#' Use \link\code{preprocess_COSMOS} to prepare the prior knowledge network or 
+#' load the one in the toolbox.  
+#'  
+#' @param meta_network prior knowledge network. 
+#' @param signaling_data numerical vector, where names are signaling nodes 
+#' in the PKN and values are from \{1, 0, -1\}. Continuous data will be 
+#' discretized using the \link\code{sign} function.  
+#' @param metabolic_data numerical vector, where names are metabolic nodes 
+#' in the PKN and values are continuous values that represents log2 fold change 
+#' or t values from a differential analysis. These values are compared to 
+#' the simulation results (simulated nodes can take value -1, 0 or 1)
+#' @param solver_path argument passed to \link\code{CARNIVAL::runCARNIVAL}. 
+#' used if filter_tf_gene_interaction_by_optimization is TRUE
+#' @param solver argument passed to \link\code{CARNIVAL::runCARNIVAL}
+#' used if filter_tf_gene_interaction_by_optimization is TRUE
+#' @param time_limit argument passed to \link\code{CARNIVAL::runCARNIVAL}
+#' used if filter_tf_gene_interaction_by_optimization is TRUE
+#' @export
+#' @import dplyr
+#' @return named list with the following members: 
+#'  - `meta_network`  filtered PKN
+#'  - `tf_regulon`  TF - target regulatory network
+#'  - `signaling_data_bin` binarised signaling data 
+#'  - `metabolic_data`  metabolomics data
+#'  - `optimized_network` initial optimized network if filter_tf_gene_interaction_by_optimization is TRUE. 
+
+
+run_COSMOS_signaling_to_metabolism <- function(meta_network,
+                                               signaling_data,
+                                               metabolic_data,
+                                               solver_path = NULL, 
+                                               solver = "cplex",
+                                               time_limit = 7200,
+                                               test_run = FALSE){
+    
+    ## Checking COSMOS input format
+    check_COSMOS_inputs(meta_network = meta_network,
+                        signaling_data = signaling_data,
+                        metabolic_data = metabolic_data)
+    
+    check_gene_names(signaling_data)
+    
+    # Check overlap among node names in the inputs
+    check_network_data_coverage(meta_network = meta_network,
+                                signaling_data = signaling_data,
+                                metabolic_data = metabolic_data)
+    
+    if(!test_run){
+        CARNIVAL_results = runCARNIVAL_wrapper(network = meta_network,
+                                               input_data = sign(signaling_data),
+                                               measured_data = measured_data,
+                                               solver_path = solver_path,
+                                               solver = solver,
+                                               timelimit = timelimit,
+                                               mipGAP = 0.2)
+        
+    }else{
+        CARNIVAL_results <- cosmos:::CARNIVAL_results_up_down
+    }
+    
+    network_results <- process_CARNIVAL_results(CARNIVAL_results)
+    
+    
+}
+
+
